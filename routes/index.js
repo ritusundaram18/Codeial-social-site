cf91ac48bd970f03783745462c1161457f5a9208
unknown <ritusundaram.05@gmail.com>
2021-04-18 14:38:59 +0530

    setting up the view engine

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ const homeController=require('../controller/home_controller');
 console.log('router loaded');
 
 router.get('/', homeController.home);
+router.use('/users',require('./users'));
+
+// for any futher routes access from here
+//route.use('/router name',require('./routerfile));
 
  module.exports=router;
